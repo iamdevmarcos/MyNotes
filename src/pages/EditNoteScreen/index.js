f2e75808f6118dc43a394e5c5644693a005ef869
Marcos Andre <marcosdev.me@gmail.com>
2022-01-30 06:37:24 -0300

    feat: add edit page styles

--- src/pages/EditNoteScreen/index.js
+++ src/pages/EditNoteScreen/index.js
@@ -1,10 +1,27 @@
-import React from "react";
-import { Container, Text } from "./styles";
+import React, { useState } from "react";
+import { Container, TitleInput, BodyInput } from "./styles";
 
 export default () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
   return (
     <Container>
-      <Text style={{ fontFamily: "Poppins-SemiBold" }}>EditNoteScreen</Text>
+      <TitleInput
+        value={title}
+        onChangeText={(t) => setTitle(t)}
+        placeholder="Digite o titulo da anotação"
+        placeholderTextColor="#ccc"
+        autoFocus={true}
+      />
+      <BodyInput
+        value={body}
+        onChangeText={(t) => setBody(t)}
+        placeholder="Digite o corpo da anotação"
+        placeholderTextColor="#ccc"
+        multiline={true}
+        textAlignVertical="top"
+      />
     </Container>
   );
 };
--- src/pages/EditNoteScreen/styles.js
+++ src/pages/EditNoteScreen/styles.js
@@ -1,4 +1,20 @@
 import styled from "styled-components/native";
 
-export const Container = styled.View``;
-export const Text = styled.Text``;
+export const Container = styled.View`
+  flex: 1;
+  background: #333;
+`;
+
+export const TitleInput = styled.TextInput`
+  font-size: 20px;
+  font-weight: bold;
+  padding: 15px;
+  color: #fff;
+`;
+
+export const BodyInput = styled.TextInput`
+  flex: 1;
+  padding: 15px;
+  font-size: 15px;
+  color: #fff;
+`;
