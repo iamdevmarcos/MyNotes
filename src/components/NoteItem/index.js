039b7ba40426f7332442c3d0ae6313980f51669e
Marcos Andre <marcosdev.me@gmail.com>
2022-01-31 16:43:40 -0300

    feat: add check button

--- src/components/NoteItem/index.js
+++ src/components/NoteItem/index.js
@@ -1,17 +1,22 @@
 import React from "react";
-import { Box, Title } from "./styles";
+import { Box, BoxContainer, Title, Check, CheckImage } from "./styles";
 
 export default ({ data, index, onPress }) => {
   return (
     <Box onPress={() => onPress(index)}>
-      <Title
-        style={{
-          fontFamily: "WorkSans-SemiBold",
-        }}
-        success={data.done}
-      >
-        {data.title}
-      </Title>
+      <BoxContainer>
+        <Title
+          style={{
+            fontFamily: "WorkSans-SemiBold",
+          }}
+          success={data.done}
+        >
+          {data.title}
+        </Title>
+        <Check>
+          <CheckImage source={require("../../assets/check.png")} />
+        </Check>
+      </BoxContainer>
     </Box>
   );
 };
--- src/components/NoteItem/styles.js
+++ src/components/NoteItem/styles.js
@@ -2,12 +2,16 @@ import styled from "styled-components/native";
 
 export const Box = styled.TouchableHighlight`
   padding: 15px;
-  border-style: solid;
-  border-bottom-color: #222;
-  border-bottom-width: 1px;
-  /* background: tomato; */
-  /* margin: 10px; */
-  /* border-radius: 30px; */
+  background: #f5f9f8;
+  margin: 10px;
+  border-radius: 5px;
+`;
+
+export const BoxContainer = styled.View`
+  /* background: #0000ff; */
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 export const Title = styled.Text`
@@ -15,4 +19,16 @@ export const Title = styled.Text`
   color: #000;
   text-decoration: ${(props) =>
     props.success === true ? "line-through" : "none"};
+`;
+
+export const Check = styled.TouchableHighlight`
+  /* background: red; */
+  width: 30px;
+  height: 30px;
+  align-items: center;
+  justify-content: center;
+`;
+export const CheckImage = styled.Image`
+  width: 25px;
+  height: 25px;
 `;
